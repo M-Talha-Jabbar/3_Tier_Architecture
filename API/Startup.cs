fc128be8dd6cd2e608b955cc1f9b644d8000fe49
M. Talha Jabbar <muhammadtalha61940@gmail.com>
2022-08-06 12:07:06 +0500

    added authorization with swagger - accepting bearer token (i.e. in Authorization Header)

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Repository.Data;
 using Repository.Repositories;
 using Service.Contracts;
 using Service.Services;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 
 namespace API
@@ -57,6 +58,18 @@ namespace API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+
+                // Configuring Authorization with Swagger - Accepting Bearer Token (i.e. in Authentication Header)
+                // Alternative of this could be simply using Postman.
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization Header using the Bearer scheme {\"bearer {token}\"}",
+                    In = ParameterLocation.Header, // The Locaton of the ApiKey. Valid values are "query", "header", and "cookie".
+                    Name = "Authorization", // The name of the query, header or cookie parameter to be used.
+                    Type = SecuritySchemeType.ApiKey, // The type of the security scheme. Valid values are "apiKey", "http", "oauth2", "openIdConnect"
+                });
+
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
             });
         }
 
